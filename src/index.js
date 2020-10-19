7ae19712d02a7873cc313e9d0fe7d044aee185a4
Ashray Shetty <ashray.shetty@empired.com>
2020-10-20 09:02:38 +1100

    small quick update to hide button on click

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import 'bootstrap';
 
 function showMoreCards() {
   document.getElementById('moreCards').classList.remove('d-none');
+  document.getElementById('showMoreCardsButton').classList.add('d-none');
 }
 
 window.showMoreCards = showMoreCards;
